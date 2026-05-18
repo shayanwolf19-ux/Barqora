7a9ab4d915f8fc124bcaa3618c14b931f58b6575
maham5 <mahamf492@gmail.com>
2026-05-18 17:56:53 +0500

    comit

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
-
+export const runtime = "nodejs"; 
 export async function POST(request: Request) {
   try {
     const { name, email, message } = await request.json();
